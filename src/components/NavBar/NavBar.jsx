b1295acb5917d6a273bb9f12861c18b51cf3abd1
shivamshaw07 <shivamshaw9005@gmail.com>
2023-11-29 10:47:41 +0530

    responsive navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,17 +8,17 @@ const NavBar = () => {
   const [navActive, setNavActive] = useState(false);
   return (
     <div className="relative w-[100vw] border-b-2 bg-white">
-      <div className="w-[95%] md:w-[95%] lg:w-full xl:w-full h-[10vh] flex justify-between items-center mx-auto px-0 md:px-0 lg:px-[10vw] xl:px-[10vw] ">
+      <div className="w-[95%] md:w-[95%] lg:w-full xl:w-full h-[10vh] flex justify-center md:justify-center lg:justify-between xl:justify-between items-center mx-auto px-0 md:px-0 lg:px-[10vw] xl:px-[10vw] ">
         <div className="flex gap-2 justify-center items-center">
           {!navActive && (
             <FaBars
-              className="font-semibold text-xl block md:block lg:hidden xl:hidden"
+              className="font-semibold text-xl block md:block lg:hidden xl:hidden absolute left-3"
               onClick={() => setNavActive((prev) => !prev)}
             />
           )}
           <Link to="/">
             <h1
-              className="font-semibold text-2xl md:text-2xl lg:text-4xl xl:text-4xl"
+              className="font-semibold text-2xl md:text-2xl lg:text-4xl xl:text-4xl "
               style={{ WebkitTextStroke: ".3px #0d7a5f" }}
             >
               EZService Hub
@@ -48,7 +48,7 @@ const NavBar = () => {
             </li>
             <li>
               <Link to="/become-a-tasker">
-                <button className="font-semibold  text-sm md:text-sm lg:text-[17px] xl:text-[17px] border border-[#0d7a5f] hover:bg-[#0d7a5f] hover:text-white transition-all duration-300 text-[#0d7a5f] px-3 md:px-3 lg:px-8 xl:px-8 rounded-2xl py-1 md:py-1 lg:py-2 xl:py-2">
+                <button className="font-semibold hidden md:hidden lg:block xl:block  text-sm md:text-sm lg:text-[17px] xl:text-[17px] border border-[#0d7a5f] hover:bg-[#0d7a5f] hover:text-white transition-all duration-300 text-[#0d7a5f] px-3 md:px-3 lg:px-8 xl:px-8 rounded-2xl py-1 md:py-1 lg:py-2 xl:py-2">
                   Become a Tasker
                 </button>
               </Link>
@@ -57,7 +57,7 @@ const NavBar = () => {
         </div>
       </div>
       {navActive && (
-        <div className="w-full z-50 top-0 absolute h-[100vh] overflow-hidden block md:block lg:hidden xl:hidden ">
+        <div className="w-full z-50 top-0 absolute h-[110vh] overflow-hidden block md:block lg:hidden xl:hidden ">
           <div className="w-full h-full flex fixed top-0">
             <div className="w-[85vw] flex bg-white p-4">
               <div className="flex flex-col gap-5 justify-start mx-auto w-full">
@@ -78,6 +78,9 @@ const NavBar = () => {
                 </Link>
                 <Link to={'/signin'}>
                     <div className="text-[#386842] border-b-2 font-semibold w-full text-xl pl-2 pb-5">Sign in</div>
+                </Link>
+                <Link to={'/become-a-tasker'}>
+                    <div className="text-[#386842] border-b-2 font-semibold w-full text-xl pl-2 pb-5">Become a Tasker</div>
                 </Link>
               </div>
             </div>
